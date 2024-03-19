559d5010debd882c65997a16773c5f81f81a543b
12698 <lyshgd@gmail.com>
2024-03-19 21:16:28 +0800

    无

--- 有意思的小算法/一个产生素数的算法.py
+++ 有意思的小算法/一个产生素数的算法.py
@@ -23,20 +23,51 @@
 
 
 # 产生素数的改进算法 ,开根算法极大的减少了运算量
+# import math
+#
+#
+# def Prime(n):
+#     prime_list = list(range(1, n + 1))
+#     for i in range(2, int(math.sqrt(n)) + 1):
+#         for j in prime_list:
+#             if j % i == 0 and j != i:
+#                 prime_list.remove(j)
+#     return prime_list
+#
+#
+# with open('prime.txt', 'w') as f:
+#     list1 = Prime(1000000)
+#     n = 1
+#     for i in list1:
+#         if n % 20 == 0:
+#             f.write(str(i) + '\n')
+#         else:
+#             f.write(str(i) + ',')
+#         n += 1
+
+
+# 算法再改进 我靠这算法的效率简直就是超级加倍！！！
 import math
 
 
 def Prime(n):
-    prime_list = list(range(1, n + 1))
-    for i in range(2, int(math.sqrt(n)) + 1):
-        for j in prime_list:
-            if j % i == 0 and j != i:
-                prime_list.remove(j)
+    prime_list = [2]
+    for i in range(3, n + 1):
+        flag = True  # 增加一个变量用于判断是否为质数
+        if i % 2 != 0:
+            for j in range(2, int(math.sqrt(i)) + 1):
+                if i % j == 0:
+                    flag = False
+                    break  # 跳出内部循环
+        else:
+            flag = False
+        if flag:
+            prime_list.append(i)
     return prime_list
 
 
 with open('prime.txt', 'w') as f:
-    list1 = Prime(1000000)
+    list1 = Prime(100000000)
     n = 1
     for i in list1:
         if n % 20 == 0:
