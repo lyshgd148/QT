64f89c010d0b6658d40b2d8c0385606b9d0f10de
12698 <lyshgd@gmail.com>
2024-03-19 19:04:05 +0800

    无

---
+++ 有意思的小算法/一个产生素数的算法.py
@@ -0,0 +1,38 @@
+# 产生一个 <N 的所有素数
+
+# def Prime(n):
+#     prime_list = list(range(1, n + 1))
+#     for i in range(2, n // 2 + 1):
+#         for j in prime_list:
+#             if j % i == 0 and j != i:
+#                 prime_list.remove(j)
+#     return prime_list
+#
+#
+# with open('prime.txt', 'w') as f:
+#     list1 = Prime(100000)
+#     n = 1
+#     for i in list1:
+#         if n % 10 == 0:
+#             f.write(str(i) + '\n')
+#         else:
+#             f.write(str(i) + ',')
+#         n += 1
+
+# 哈哈先生成10w个简单测试一下,牛逼
+
+
+# 产生一个素数的改进算法
+import math
+
+
+def Prime(n):
+    prime_list = list(range(1, n + 1))
+    for i in range(2, int(math.sqrt(n)) + 1):
+        for j in prime_list:
+            if j % i == 0 and j != i:
+                prime_list.remove(j)
+    return prime_list
+
+
+print(Prime(100000))
