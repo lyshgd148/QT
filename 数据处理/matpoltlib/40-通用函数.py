ba5d4771b995bc62932d00181fab0ac01610858a
12698 <lyshgd@gmail.com>
2024-03-23 17:06:48 +0800

    无

--- 数据处理/matpoltlib/40-通用函数.py
+++ 数据处理/matpoltlib/40-通用函数.py
@@ -1,7 +1,22 @@
 import numpy as np
 
-n = 32
-# 矩阵
-F = np.mat('1 1 ;1 0')
-for i in range(1, n):
-    print((F ** i)[0, 0])
+# n = 32
+# # 矩阵
+# F = np.mat('0 1 ;1 -1')
+# # for i in range(1, n):
+# #     print((F ** i)[0, 0])
+#
+# print(F ** 19)
+#
+# F = np.mat('1 1 ;1 0')
+# print(F ** -19)
+
+
+a = np.arange(1, 10)
+# 数组的裁剪  ndarray.clip(min=,max=) 返回数组 原来的数组不变
+print(np.clip(a, 3, 8))
+
+# 数组的压缩  ndarray.compress(条件) 返回满足体条件的元素组成的新数组
+print(np.compress(a > 5, a))
+print(a[a > 5])
+print(a[(a > 3) & (a <= 7)])
--- 数据处理/matpoltlib/41-三角函数.py
+++ 数据处理/matpoltlib/41-三角函数.py
@@ -3,11 +3,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-n = 100
-x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
-y = np.zeros(1000)
+n = 100000
+x = np.linspace(-2 * np.pi, 2 * np.pi, 10000)
+y = np.zeros(10000)
 for i in range(1, n + 1):
-    y += 4 * np.pi / (2 * n - 1) * np.sin((2 * n - 1) * x)
+    y += ((4 * np.pi / (2 * i - 1)) * np.sin((2 * i - 1) * x))
 
 plt.figure()
 plt.plot(x, y)
