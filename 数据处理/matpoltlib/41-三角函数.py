2f8c8b0e2bf19a42131792c14a0264e8e29bb8b2
12698 <lyshgd@gmail.com>
2024-03-25 21:54:43 +0800

    无

--- 数据处理/matpoltlib/41-三角函数.py
+++ 数据处理/matpoltlib/41-三角函数.py
@@ -1,13 +1,26 @@
-# 用三角函数 合成方波
+# # 用三角函数 合成方波
+#
+# import numpy as np
+# import matplotlib.pyplot as plt
+#
+# n = 100000
+# x = np.linspace(-2 * np.pi, 2 * np.pi, 10000)
+# y = np.zeros(10000)
+# for i in range(1, n + 1):
+#     y += ((4 * np.pi / (2 * i - 1)) * np.sin((2 * i - 1) * x))
+#
+# plt.figure()
+# plt.plot(x, y)
+# plt.show()
 
+
+# 采样定理的一些测试
 import numpy as np
 import matplotlib.pyplot as plt
 
-n = 100000
-x = np.linspace(-2 * np.pi, 2 * np.pi, 10000)
-y = np.zeros(10000)
-for i in range(1, n + 1):
-    y += ((4 * np.pi / (2 * i - 1)) * np.sin((2 * i - 1) * x))
+n = 1000
+x = np.linspace(0, 1, 50000)
+y = np.sin(n * 2 * np.pi * x)
 
 plt.figure()
 plt.plot(x, y)
