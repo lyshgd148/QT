53336765d73ed937e09b3fac6e4326b7dc14869b
12698 <lyshgd@gmail.com>
2024-03-30 23:46:54 +0800

    无

--- 有意思的小算法/冒泡排序法.py
+++ 有意思的小算法/冒泡排序法.py
@@ -1,7 +1,7 @@
-#冒泡排序法(从小到大)
+# 冒泡排序法(从小到大)
 def sort_list_small(list_):
-    n=len(list_)
-    for i in range(n-1):
-        for j in range(n-1-i):
-            if list_[j+1]<=list_[j]:
-                list_[j+1],list_[j]=list_[j],list_[j+1]
+    n = len(list_)
+    for i in range(n - 1):
+        for j in range(n - 1 - i):
+            if list_[j + 1] <= list_[j]:
+                list_[j + 1], list_[j] = list_[j], list_[j + 1]
