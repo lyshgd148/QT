e9a7be9a7c311f93630d57c4c5df81aff122073e
12698 <lyshgd@gmail.com>
2024-03-31 17:31:47 +0800

    无

--- 有意思的小算法/椭圆光学性质-反射轨迹.py
+++ 有意思的小算法/椭圆光学性质-反射轨迹.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 class Ellipse:
-    def __init__(self, k):
+    def __init__(self, k, n):
         # 生成椭圆数据
         x = np.linspace(-5, 5, 5000)  # x 坐标范围
         y = np.linspace(-4, 4, 4000)  # y 坐标范围
@@ -12,13 +12,13 @@ class Ellipse:
         self.x = [-3]
         self.y = [0]
         self.k = k
-        self.n = 25
+        self.n = n
         x1 = (-150 * self.k ** 2 + np.sqrt(25600 * (self.k ** 2 + 1))) / (50 * self.k ** 2 + 32)
         y1 = self.k * (x1 + 3)
         self.x.append(x1)
         self.y.append(y1)
 
-    def draw_ellipse(self):
+    def data_ellipse(self):
         for i in range(self.n):
             if i % 2 == 0:
                 k = self.y[i + 1] / (self.x[1 + i] - 3)
@@ -44,11 +44,24 @@ class Ellipse:
                     self.x.append(x2)
                 self.y.append(y_)
 
+    def draw_ellipse(self):
         plt.contour(self.X, self.Y, self.Z, [1])
         plt.scatter([-3, 3], [0, 0], color='orangered', zorder=3)
-        plt.plot(self.x, self.y)
         plt.axis('equal')
         plt.xlim(-7, 7)
         plt.ylim(-5, 5)
-
+        for i in range(self.n):
+            color = np.random.rand(3)
+            plt.plot(self.x[i:i + 2], self.y[i:i + 2], color=color)
+            plt.pause(0.25)
+            # plt.cla()
+        plt.plot(self.x, self.y)
         plt.show()
+
+    def main(self):
+        self.data_ellipse()
+        self.draw_ellipse()
+
+
+if __name__ == "__main__":
+    Ellipse(1, 20).main()
