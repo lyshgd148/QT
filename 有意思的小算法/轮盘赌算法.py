063b7e56ea61d4ffc916a88be6973a971ebfe2b3
12698 <lyshgd@gmail.com>
2024-04-20 13:48:24 +0800

    无

---
+++ 有意思的小算法/轮盘赌算法.py
@@ -0,0 +1,30 @@
+import random
+def roulette(rate):
+    rand_num = random.random()
+    rate_new = list()
+    index = 0
+    test = rate[0]
+    rate_new.append(rate[0])
+    for i in range(1, len(rate)):
+        test += rate[i]
+        rate_new.append(test)
+    for i, j in enumerate(rate_new):
+        if rand_num <= j:
+            index = i
+            break
+    return index
+
+
+rate = [0.2, 0.1, 0.2, 0.4, 0.1]
+
+temp = list()
+temp_num = []
+for i in range(100000):
+    num = roulette(rate)
+    temp.append(num)
+
+for i in range(5):
+    temp_num.append(temp.count(i))
+
+print(temp)
+print(temp_num)
