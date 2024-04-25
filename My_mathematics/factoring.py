fd9bc8cf6ec05813ce5d08a9b8f3806af300113e
12698 <lyshgd@gmail.com>
2024-04-26 03:43:00 +0800

    无

--- My_mathematics/factoring.py
+++ My_mathematics/factoring.py
@@ -57,6 +57,8 @@ def factoring(numerator, denominator):
     print('\n')
     string_num1 = 0
     string_num2 = 0
+    string_num3 = 0
+    string_num4 = 0
     for i in range(n1):
         if i != n1 - 1:
             if i == 0:
@@ -146,6 +148,91 @@ def factoring(numerator, denominator):
             s = " + "
             string_num2 += len(s)
 
+    for i in range(len(temp)):
+        if i != len(temp) - 1:
+            if i == 0:
+                if temp[i] == 1:
+                    s = f'x^{len(temp) - 1 - i}'
+                    string_num3 += len(s)
+                elif temp[i] == -1:
+                    s = f'-x^{len(temp) - 1 - i}'
+                    string_num3 += len(s)
+                else:
+                    if temp[i] != 0:
+                        s = f'{temp[i]}x^{len(temp) - 1 - i}'
+                        string_num3 += len(s)
+            if i != 0 and temp[i] > 0:
+                if temp[i] == 1:
+                    s = f'+x^{len(temp) - 1 - i}'
+                    string_num3 += len(s)
+                else:
+                    s = f'+{temp[i]}x^{len(temp) - 1 - i}'
+                    string_num3 += len(s)
+            elif i != 0 and temp[i] < 0:
+                if temp[i] == -1:
+                    s = f'-x^{len(temp) - 1 - i}'
+                    string_num3 += len(s)
+                else:
+                    s = f'{temp[i]}x^{len(temp) - 1 - i}'
+                    string_num3 += len(s)
+        else:
+            if temp[i] > 0:
+                if temp[i] == 1:
+                    s = f'+x^{len(temp) - 1 - i}'
+                    string_num3 += len(s)
+                else:
+                    s = f'+{temp[i]}x^{len(temp) - 1 - i}'
+                    string_num3 += len(s)
+            elif temp[i] < 0:
+                if temp[i] == -1:
+                    s = f'-x^{len(temp) - 1 - i}'
+                    string_num3 += len(s)
+                else:
+                    s = f'{temp[i]}x^{len(temp) - 1 - i}'
+                    string_num3 += len(s)
+
+    for i in range(len(denominator)):
+        if i != len(denominator) - 1:
+            if i == 0:
+                if denominator[i] == 1:
+                    s = f'x^{len(denominator) - 1 - i}'
+                    string_num4 += len(s)
+                elif denominator[i] == -1:
+                    s = f'-x^{len(denominator) - 1 - i}'
+                    string_num4 += len(s)
+                else:
+                    s = f'{denominator[i]}x^{len(denominator) - 1 - i}'
+                    string_num4 += len(s)
+            if i != 0 and denominator[i] > 0:
+                if denominator[i] == 1:
+                    s = f'+x^{len(denominator) - 1 - i}'
+                    string_num4 += len(s)
+                else:
+                    s = f'+{denominator[i]}x^{len(denominator) - 1 - i}'
+                    string_num4 += len(s)
+            elif i != 0 and denominator[i] < 0:
+                if denominator[i] == -1:
+                    s = f'-x^{len(denominator) - 1 - i}'
+                    string_num4 += len(s)
+                else:
+                    s = f'{denominator[i]}x^{len(denominator) - 1 - i}'
+                    string_num4 += len(s)
+        else:
+            if denominator[i] > 0:
+                if denominator[i] == 1:
+                    s = f'+x^{len(denominator) - 1 - i}'
+                    string_num4 += len(s)
+                else:
+                    s = f'+{denominator[i]}x^{len(denominator) - 1 - i}'
+                    string_num4 += len(s)
+            elif denominator[i] < 0:
+                if denominator[i] == -1:
+                    s = f'-x^{len(denominator) - 1 - i}'
+                    string_num4 += len(s)
+                else:
+                    s = f'{denominator[i]}x^{len(denominator) - 1 - i}'
+                    string_num4 += len(s)
+
     for i in range(n1):
         if i != n1 - 1:
             if i == 0:
@@ -177,7 +264,45 @@ def factoring(numerator, denominator):
                     print(f'-x^{n1 - 1 - i}', end='')
                 else:
                     print(f'{numerator[i]}x^{n1 - 1 - i}', end='')
-            print(" = ", end='')
+
+    for i in range(string_num2 + 6):
+        print(' ', end='')
+    for i in range(len(temp)):
+        if i != len(temp) - 1:
+            if i == 0:
+                if temp[i] == 1:
+                    print(f'x^{len(temp) - 1 - i}', end='')
+                elif temp[i] == -1:
+                    print(f'-x^{len(temp) - 1 - i}', end='')
+                else:
+                    if temp[i] != 0:
+                        print(f'{temp[i]}x^{len(temp) - 1 - i}', end='')
+            if i != 0 and temp[i] > 0:
+                if temp[i] == 1:
+                    print(f'+x^{len(temp) - 1 - i}', end='')
+                else:
+                    print(f'+{temp[i]}x^{len(temp) - 1 - i}', end='')
+            elif i != 0 and temp[i] < 0:
+                if temp[i] == -1:
+                    print(f'-x^{len(temp) - 1 - i}', end='')
+                else:
+                    print(f'{temp[i]}x^{len(temp) - 1 - i}', end='')
+        else:
+            if temp[i] > 0:
+                if temp[i] == 1:
+                    print(f'+x^{len(temp) - 1 - i}', end='')
+                else:
+                    print(f'+{temp[i]}x^{len(temp) - 1 - i}', end='')
+            elif temp[i] < 0:
+                if temp[i] == -1:
+                    print(f'-x^{len(temp) - 1 - i}', end='')
+                else:
+                    print(f'{temp[i]}x^{len(temp) - 1 - i}', end='')
+
+    print('\n')
+    for i in range(string_num1):
+        print('-', end='')
+    print(' = ', end='')
 
     for i in range(len(result)):
         if i != len(result) - 1:
@@ -210,14 +335,79 @@ def factoring(numerator, denominator):
                 else:
                     print(f'{result[i]}x^{len(result) - 1 - i}', end='')
             print(" + ", end='')
+    for i in range(string_num3 + 2):
+        print('-', end='')
 
     print('\n')
-    print(string_num1, string_num2)
+    for i in range(len(denominator)):
+        if i != len(denominator) - 1:
+            if i == 0:
+                if denominator[i] == 1:
+                    print(f'x^{len(denominator) - 1 - i}', end='')
+                elif denominator[i] == -1:
+                    print(f'-x^{len(denominator) - 1 - i}', end='')
+                else:
+                    print(f'{denominator[i]}x^{len(denominator) - 1 - i}', end='')
+            if i != 0 and denominator[i] > 0:
+                if denominator[i] == 1:
+                    print(f'+x^{len(denominator) - 1 - i}', end='')
+                else:
+                    print(f'+{denominator[i]}x^{len(denominator) - 1 - i}', end='')
+            elif i != 0 and denominator[i] < 0:
+                if denominator[i] == -1:
+                    print(f'-x^{len(denominator) - 1 - i}', end='')
+                else:
+                    print(f'{denominator[i]}x^{len(denominator) - 1 - i}', end='')
+        else:
+            if denominator[i] > 0:
+                if denominator[i] == 1:
+                    print(f'+x^{len(denominator) - 1 - i}', end='')
+                else:
+                    print(f'+{denominator[i]}x^{len(denominator) - 1 - i}', end='')
+            elif denominator[i] < 0:
+                if denominator[i] == -1:
+                    print(f'-x^{len(denominator) - 1 - i}', end='')
+                else:
+                    print(f'{denominator[i]}x^{len(denominator) - 1 - i}', end='')
+    for i in range(string_num1 + string_num2 - string_num4 + 3):
+        print(' ', end='')
+
+    for i in range(len(denominator)):
+        if i != len(denominator) - 1:
+            if i == 0:
+                if denominator[i] == 1:
+                    print(f'x^{len(denominator) - 1 - i}', end='')
+                elif denominator[i] == -1:
+                    print(f'-x^{len(denominator) - 1 - i}', end='')
+                else:
+                    print(f'{denominator[i]}x^{len(denominator) - 1 - i}', end='')
+            if i != 0 and denominator[i] > 0:
+                if denominator[i] == 1:
+                    print(f'+x^{len(denominator) - 1 - i}', end='')
+                else:
+                    print(f'+{denominator[i]}x^{len(denominator) - 1 - i}', end='')
+            elif i != 0 and denominator[i] < 0:
+                if denominator[i] == -1:
+                    print(f'-x^{len(denominator) - 1 - i}', end='')
+                else:
+                    print(f'{denominator[i]}x^{len(denominator) - 1 - i}', end='')
+        else:
+            if denominator[i] > 0:
+                if denominator[i] == 1:
+                    print(f'+x^{len(denominator) - 1 - i}', end='')
+                else:
+                    print(f'+{denominator[i]}x^{len(denominator) - 1 - i}', end='')
+            elif denominator[i] < 0:
+                if denominator[i] == -1:
+                    print(f'-x^{len(denominator) - 1 - i}', end='')
+                else:
+                    print(f'{denominator[i]}x^{len(denominator) - 1 - i}', end='')
+
     return result, temp
 
 
 if __name__ == "__main__":
-    temp = LU_decompose.LU_decompose([[1, 2, 3], [3, 2, 5], [4, 6, 7]], [2, 5, 1])
-    print(temp)
+    # temp = LU_decompose.LU_decompose([[1, 2, 3], [3, 2, 5], [4, 6, 7]], [2, 5, 1])
+    # print(temp)
     temp = factoring([1, -4, 6, -4, 1, 0, 0, 0, 0], [1, 0, 1])
     # print(temp)
