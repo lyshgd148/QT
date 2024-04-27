38ee373aed83c978baf937d0e3ca55462a204dca
12698 <lyshgd@gmail.com>
2024-04-27 22:46:19 +0800

    无

--- 有意思的小算法/BFS算法.py
+++ 有意思的小算法/BFS算法.py
@@ -20,27 +20,34 @@ image_new = copy.deepcopy(image)
 temp = list()
 
 
-def bfs(i, j, image):
-    if i - 1 >= 0 and image[i - 1][j] == 1:
+def bfs(i, j):
+    global temp
+    if i - 1 >= 0 and image_new[i - 1][j] == 1:
         temp.append((i - 1, j))
-        image[i - 1][j] = 2
-        temp.append((i - 1, j))
-    elif j - 1 >= 0 and image[i][j - 1] == 1:
+        image_new[i - 1][j] = 2
+    if j - 1 >= 0 and image_new[i][j - 1] == 1:
         temp.append((i, j - 1))
-        image[i][j - 1] = 2
-        temp.append((i, j - 1))
-    elif j + 1 <= len(image[0]) and image[i][j + 1] == 1:
+        image_new[i][j - 1] = 2
+    if j + 1 <= len(image[0]) and image_new[i][j + 1] == 1:
         temp.append((i, j + 1))
-        image[i][j + 1] = 2
-    elif i + 1 < len(image) and image[i + 1][j] == 1:
+        image_new[i][j + 1] = 2
+    if i + 1 < len(image) and image_new[i + 1][j] == 1:
         temp.append((i + 1, j))
-        image[i + 1][j] = 2
+        image_new[i + 1][j] = 2
+
+    if len(temp) !=0:
+        i, j = temp.pop(0)
+        bfs(i, j)
+    else:
+        return
 
 
 num = 0
 for i in range(h):
     for j in range(w):
-        pass
+        if image_new[i][j] == 1:
+            num += 1
+            bfs(i, j)
 
 print(f'图像中的连同区域有{num}个')
 
