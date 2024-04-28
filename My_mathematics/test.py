cfc8f15d049580ef5245c54f247213a8cf85d046
12698 <lyshgd@gmail.com>
2024-04-28 15:33:27 +0800

    无

--- My_mathematics/test.py
+++ My_mathematics/test.py
@@ -1,7 +1,6 @@
 import matrix
 import LU_decompose
 
-
-if __name__=='__main__':
-    temp=LU_decompose.LU_decompose([[-1,-1],[-1,0]],[-8,5])
+if __name__ == '__main__':
+    temp = LU_decompose.LU_decompose([[-1, -1], [-1, 0]], [-8, 5])
     print(temp)
