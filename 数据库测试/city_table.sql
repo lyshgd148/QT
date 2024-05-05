3ed46289c5e8b189813623b0c446c3776faf6eec
12698 <lyshgd@gmail.com>
2024-05-06 00:16:19 +0800

    无

---
+++ 数据库测试/city_table.sql
@@ -0,0 +1,18 @@
+create table city
+(
+    NanJing     double null,
+    WuXi        double null,
+    XuZhou      double null,
+    ChangZhou   double null,
+    SuZhou      double null,
+    NanTong     double null,
+    LianYunGang double null,
+    HuaiAn      double null,
+    YanCheng    double null,
+    YangZhou    double null,
+    ZhenJiang   double null,
+    TaiZhou     double null,
+    SuQian      double null
+);
+
+
