56474249822468b8296213c6568bb23989553e03
12698 <lyshgd@gmail.com>
2024-05-22 05:26:04 +0800

    无

--- BP神经网络/picture_process.py
+++ BP神经网络/picture_process.py
@@ -1,16 +1,17 @@
 import cv2
 
-image = cv2.imread("training_data.jpg", cv2.IMREAD_GRAYSCALE)
-# cv2.imwrite("training_data.jpg", image)
+path="9.jpg"
+image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+cv2.imwrite(path, image)
 print(image.shape)
 
-m = len(image)
-n = len(image[0])
-print(m / 28, n / 28)
-
-num = 1
-for i in range(0, m // 28):
-    for j in range(n // 28):
-        image_block = image[i * 28:i * 28 + 28, j * 28:j * 28 + 28]
-        cv2.imwrite(f"./picture/{num}.jpg", image_block)
-        num += 1
+# m = len(image)
+# n = len(image[0])
+# print(m / 28, n / 28)
+#
+# num = 1
+# for i in range(0, m // 28):
+#     for j in range(n // 28):
+#         image_block = image[i * 28:i * 28 + 28, j * 28:j * 28 + 28]
+#         cv2.imwrite(f"./picture/{num}.jpg", image_block)
+#         num += 1
