13d024f55dd462e4506195a6bf1af683d682b151
12698 <lyshgd@gmail.com>
2024-07-21 04:42:57 +0800

    无

--- 有意思的小算法/微分方程组.py
+++ 有意思的小算法/微分方程组.py
@@ -5,15 +5,15 @@ from scipy.integrate import odeint
 
 def f(t, u):
     x, y = u
-    dxdt = ((x - t) / L ** 2) * (x - t + 2 * t * y - 2 * t ** 3)
-    dydt = ((y - t ** 2) / L ** 2) * (x - t + 2 * t * y - 2 * t ** 3)
+    dxdt = ((x - t) / L ** 2) * (x - t + (y - np.log(1 + t)) / (1 + t))
+    dydt = ((y - np.log(1 + t)) / L ** 2) * (x - t + (y - np.log(1 + t)) / (1 + t))
     dudt = [dxdt, dydt]
     return dudt
 
 L = 1
 u0 = [-L, 0]
-t = np.linspace(0, 10, 1000)
-front = t ** 2
+t = np.linspace(0, 5, 10000)
+front = np.log(1 + t)
 sol = odeint(f, u0, t, tfirst=True)
 
 plt.plot(sol[:, 0], sol[:, 1], color='r')
