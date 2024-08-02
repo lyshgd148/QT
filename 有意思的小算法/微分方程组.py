7321c0102e75364dcc7acfde1ffd354f89d3b672
12698 <lyshgd@gmail.com>
2024-08-02 23:53:18 +0800

    无

--- 有意思的小算法/微分方程组.py
+++ 有意思的小算法/微分方程组.py
@@ -10,13 +10,35 @@ def f(t, u):
     dudt = [dxdt, dydt]
     return dudt
 
+
+def inital(ang, l):
+    return l * np.cos(ang), l * np.sin(ang)
+
+
 L = 1
-u0 = [-L, 0]
-t = np.linspace(0, 5, 10000)
+u0 = inital(np.pi, L)
+t = np.linspace(0, 8, 10000)
 front = np.log(1 + t)
 sol = odeint(f, u0, t, tfirst=True)
 
 plt.plot(sol[:, 0], sol[:, 1], color='r')
+
+u0 = inital(np.pi * 5 / 4, L)
+sol = odeint(f, u0, t, tfirst=True)
+plt.plot(sol[:, 0], sol[:, 1], color='r')
+
+u0 = inital(np.pi * 7 / 4, L)
+sol = odeint(f, u0, t, tfirst=True)
+plt.plot(sol[:, 0], sol[:, 1], color='r')
+
+u0 = inital(np.pi / 2, L)
+sol = odeint(f, u0, t, tfirst=True)
+plt.plot(sol[:, 0], sol[:, 1], color='r')
+
+u0 = inital(np.pi * 0, L)
+sol = odeint(f, u0, t, tfirst=True)
+plt.plot(sol[:, 0], sol[:, 1], color='r')
+
 plt.plot(t, front, color='b')
 plt.grid()
 plt.show()
