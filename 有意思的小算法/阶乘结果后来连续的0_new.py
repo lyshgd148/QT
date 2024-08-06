0ffcc9e22bddf1f3cdc008b2441a64a6ac1e92b0
12698 <lyshgd@gmail.com>
2024-08-07 01:20:51 +0800

    无

---
+++ 有意思的小算法/阶乘结果后来连续的0_new.py
@@ -0,0 +1,23 @@
+# 改版计算阶乘结果末尾几个连续的零
+def Cum_NUm(n):
+    num = 0
+    time = 1
+    while n // (5 ** time):
+        num += (n // (5 ** time))
+        time += 1
+    return num
+
+
+end = 1000
+num = list()
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(1, end, end)
+for i in range(1, end + 1):
+    num.append(Cum_NUm(i))
+
+plt.plot(x, num)
+plt.grid()
+plt.show()
