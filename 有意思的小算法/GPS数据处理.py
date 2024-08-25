5183d46e56013ed7693349ceab5b885fa4145545
12698 <lyshgd@gmail.com>
2024-08-26 00:14:00 +0800

    无

--- 有意思的小算法/GPS数据处理.py
+++ 有意思的小算法/GPS数据处理.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import pandas as pd
 import math
 
 data = pd.read_excel('./GPS.xlsx')
 r = 6357
-width = list()
-height = list()
+width = [0]
+height = [0]
 x = list()
 y = list()
 z = list()
@@ -14,19 +15,38 @@ time = list(data[data.columns[0]])
 latitude = list(data[data.columns[1]])  # 纬度
 longitude = list(data[data.columns[2]])  # 经度
 altitude = list(data[data.columns[3]])  # 海拔
-print(len(latitude), len(longitude), len(altitude))
 
 for i in range(len(time)):
     x.append(r * math.cos(math.radians(longitude[i])))
     y.append(r * math.sin(math.radians(longitude[i])))
     z.append(r * math.sin(math.radians(latitude[i])))
 
-length = list()
-theta = list()
+x_ = list()
+y_ = list()
 for i in range(1, len(x)):
     cos_theta = (x[i] * x[i - 1] + y[i] * y[i - 1] + z[i] * z[i - 1]) / math.sqrt(
         (x[i] ** 2 + y[i] ** 2 + z[i] ** 2) * (x[i - 1] ** 2 + y[i - 1] ** 2 + z[i - 1] ** 2))
+    length = math.sqrt(1 - cos_theta ** 2) * r * 1000
+    l = math.sqrt((longitude[i] - longitude[i - 1]) ** 2 + (latitude[i] - latitude[i - 1]) ** 2)
+    x_.append((longitude[i] - longitude[i - 1]) / l * length)
+    y_.append((latitude[i] - latitude[i - 1]) / l * length)
+    sum_x = 0
+    sum_y = 1
+    for k in range(i):
+        sum_x += x_[k]
+        sum_y += y_[k]
+    width.append(sum_x)
+    height.append(sum_y)
 
-    length.append(math.sqrt(1 - cos_theta ** 2) * r * 1000)
 
-print(length)
+def draw(x, y, z):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot(x, y, z)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_zticks([])
+    plt.show()
+
+
+draw(width, height, altitude)
