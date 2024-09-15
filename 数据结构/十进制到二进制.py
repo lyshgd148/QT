7c1f970d12b8fd90652c109fc9be16acf54b70ca
12698 <lyshgd@gmail.com>
2024-09-16 06:45:59 +0800

    无

---
+++ 数据结构/十进制到二进制.py
@@ -0,0 +1,18 @@
+from struct import Stack
+
+
+def divideByN(decNumber, N):
+    remstack = Stack()
+    digits = "0123456789ABCDEF"
+    while decNumber > 0:
+        rem = decNumber % N
+        remstack.push(rem)
+        decNumber = decNumber // N
+
+    srtings = ""
+    while not remstack.isEmpty():
+        srtings += digits[remstack.pop()]
+    return srtings
+
+
+print(divideByN(233, 16))
---
+++ 数据结构/括号匹配.py
@@ -0,0 +1,41 @@
+from struct import Stack
+
+
+def match(left, right):
+    left_ls = ['(', '[', '{']
+    right_ls = [')', ']', '}']
+    if left_ls.index(left) == right_ls.index(right):
+        return True
+    else:
+        return False
+
+
+def parChecker(symbolSring):
+    s = Stack()
+    bal = True
+    index = 0
+
+    while index < len(symbolSring) and bal:
+        if symbolSring[index] in "([{":
+            s.push(symbolSring[index])
+            # print(symbolSring[index])
+        else:
+            if s.isEmpty():
+                bal = False
+                return bal
+            else:
+                symbol = s.pop()
+                if match(symbol, symbolSring[index]):
+                    pass
+                else:
+                    return False
+        index += 1
+
+    if s.isEmpty():
+        return True
+    else:
+        return False
+
+
+print(parChecker("(((()()()()()()()()()()())))"))
+print(parChecker("(([)]())"))
