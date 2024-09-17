3f89c4850819c448817f73553d150fa5814ef875
12698 <lyshgd@gmail.com>
2024-09-18 00:09:50 +0800

    无

--- 有意思的小算法/Eular法(modify).py
+++ 有意思的小算法/Eular法(modify).py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import math
 
 
 class Eular():
@@ -9,24 +10,28 @@ class Eular():
         self.dx = dx
 
     def K(self):
-        return self.x * (1 - self.y)
+        return ((self.x * self.y) / (self.x ** 2 + self.y ** 2)) * math.exp(self.x) - self.y
 
     def f(self):
         return self.y + self.K() * self.dx
+
+    def K_(self):
+        return (((self.x + self.dx) * self.f()) / (((self.x + self.dx)) ** 2 + self.f() ** 2)) * math.exp(
+            self.x + self.dx) - self.f()
 
     def eular(self):
         temp_y = []
         temp_x = []
         for i in range(int(self.end // self.dx)):
             temp_x.append(self.x)
-            k = ((self.x + self.dx) * (1 - self.f()) + self.K()) / 2
+            k = (self.K_() + self.K()) / 2
             self.y = self.y + k * self.dx
             self.x = self.x + self.dx
             temp_y.append(self.y)
         return temp_x, temp_y
 
 
-la = Eular(0, 2, 10, 0.01)
+la = Eular(0, 350, 10, 0.0001)
 x, y = la.eular()
 
 plt.figure()
