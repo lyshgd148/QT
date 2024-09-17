83e66e1a8e5e2640b1ed93ae7e63d71c0aff8cd3
12698 <lyshgd@gmail.com>
2024-09-17 23:53:01 +0800

    无

--- 有意思的小算法/Eular法(modify).py
+++ 有意思的小算法/Eular法(modify).py
@@ -1,32 +1,34 @@
 import matplotlib.pyplot as plt
-import numpy as np
-
-# y'+xy=x   y'=x(1-y)
-x_ = 0
-y_ = 2
 
 
-def f(x, y, dx):
-    return y + x * (1 - y) * dx
+class Eular():
+    def __init__(self, x, y, end, dx):
+        self.x = x
+        self.y = y
+        self.end = end
+        self.dx = dx
+
+    def K(self):
+        return self.x * (1 - self.y)
+
+    def f(self):
+        return self.y + self.K() * self.dx
+
+    def eular(self):
+        temp_y = []
+        temp_x = []
+        for i in range(int(self.end // self.dx)):
+            temp_x.append(self.x)
+            k = ((self.x + self.dx) * (1 - self.f()) + self.K()) / 2
+            self.y = self.y + k * self.dx
+            self.x = self.x + self.dx
+            temp_y.append(self.y)
+        return temp_x, temp_y
 
 
-def Eular(x, y, dx, end):
-    temp_y = []
-    temp_x = []
-    for i in range(int(end // dx)):
-        temp_x.append(x)
-        k = ((x + dx)*(1 - f(x, y, dx)) + x * (1 - y)) / 2
-        y = y + k * dx
-        x = x + dx
-        temp_y.append(y)
-    return temp_x, temp_y
-
-
-x, y = Eular(0, 2, 0.01, 10)
+la = Eular(0, 2, 10, 0.01)
+x, y = la.eular()
 
 plt.figure()
 plt.plot(x, y)
-X = np.linspace(0, 10, 1001)
-Y = np.exp(-0.5 * X ** 2) + 1
-plt.plot(X, Y)
 plt.show()
