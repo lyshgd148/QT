478102e4107a8bcd6ea22ae09de949347dc1bd67
12698 <lyshgd@gmail.com>
2024-09-18 18:26:50 +0800

    无

--- 数据结构/中缀转后缀.py
+++ 数据结构/中缀转后缀.py
@@ -27,3 +27,31 @@ def infix2Postfix(infixexpr):
 
 
 print(infix2Postfix("A + B * ( C - D ) + E"))
+
+
+def postfixEval(postfixExpr):
+    opStack = Stack()
+    tokenList = postfixExpr.split()
+    for token in tokenList:
+        if token not in "*/+-":
+            opStack.push(int(token))
+        else:
+            op2 = opStack.pop()
+            op1 = opStack.pop()
+            result = doMath(token, op1, op2)
+            opStack.push(result)
+    return opStack.pop()
+
+
+def doMath(op, op1, op2):
+    if op == "*":
+        return op1 * op2
+    elif op == "/":
+        return op1 / op2
+    elif op == "+":
+        return op1 + op2
+    elif op == "-":
+        return op1 - op2
+
+
+print(postfixEval("7 8 + 3 2 + /"))
