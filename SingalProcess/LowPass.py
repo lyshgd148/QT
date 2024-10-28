f1ef54955faeb62b92f676be02e54eb0c23082a0
12698 <lyshgd@gmail.com>
2024-10-28 17:41:00 +0800

    无

--- SingalProcess/LowPass.py
+++ SingalProcess/LowPass.py
@@ -2,14 +2,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 import ProcessMode as md
 
-md.LowPassFilter(1, 0.01)
-data = np.arange(0, 10.01, 0.01)
+LF = md.LowPassFilter(20, 0.001)
+data = np.arange(0, 10.01, 0.001)
 
 
 def f(data):
-    return np.sin(100 * data)
+    return np.sin(100 * data) + np.cos(data) + 1.5 * np.sin(20 * data)
 
+
+y = f(data)
+y_fliter = list()
+for i in y:
+    y_fliter.append(LF.filter(i))
 
 plt.figure()
-plt.plot(data, f(data))
+plt.plot(data, y)
+plt.figure()
+plt.plot(data, y_fliter)
 plt.show()
