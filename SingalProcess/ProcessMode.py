0d2f3d26a95bf2f1cf2b80d8da01b643a4fa15c3
12698 <lyshgd@gmail.com>
2024-10-28 15:57:22 +0800

    无

--- SingalProcess/ProcessMode.py
+++ SingalProcess/ProcessMode.py
@@ -1,3 +1,16 @@
 class LowPassFilter:
+    def __init__(self, w, t):
+        self.w = w
+        self.y_0 = 0
+        self.y_last = self.y_0
+        self.T = t
+
+    def filter(self, u):
+        y = 1 / (1 + self.w * self.T) * self.y_last + ((self.w * self.T) / (1 + self.w * self.T)) * u
+        self.y_last = y
+        return y
+
+
+class HightPassFilter:
     def __init__(self):
         pass
