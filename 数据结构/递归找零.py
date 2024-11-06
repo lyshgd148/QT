67ac08fff34f0fc9c3c3f03c556b2ba65777e5b2
12698 <lyshgd@gmail.com>
2024-11-06 18:06:55 +0800

    无

--- 数据结构/递归找零.py
+++ 数据结构/递归找零.py
@@ -1,3 +1,5 @@
+import sys
+sys.setrecursionlimit(10000)
 def recDC(VlueLists, change, Results, Money):
     minCoins = change
     if change in VlueLists:
@@ -10,6 +12,7 @@ def recDC(VlueLists, change, Results, Money):
         minMoney = []
         for i in [c for c in VlueLists if change >= c]:
             numCoins, numMoney = recDC(VlueLists, change - i, Results, Money)
+            # print(Money,change,i)
             numCoins += 1
             numMoney += [i]
             if numCoins <= minCoins:
@@ -20,5 +23,5 @@ def recDC(VlueLists, change, Results, Money):
         return minCoins, minMoney #我靠这里不能返回 Money[change] 因为返回给一个变量后 其实是这个变量指向Money[change]
 
 
-num = 63
+num = 7024
 print(recDC([1, 5, 10, 25], num, [0] * (num + 1), [[] for _ in range(num + 1)]))
