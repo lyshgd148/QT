7957716c31393b968f66973a558d5f6d0d0809e2
12698 <lyshgd@gmail.com>
2024-11-08 21:55:17 +0800

    无

--- 数据结构/背包问题_递归版.py
+++ 数据结构/背包问题_递归版.py
@@ -1,6 +1,21 @@
 tr = {(2, 3), (3, 4), (4, 8), (5, 8), (9, 10)}
-m = {}
+m = {}  # 这是一个字典    (((),(),...),w):value
 
 
 def thief(tr, w):
-    pass
+    if tr == set() or w == 0:
+        m[(tuple(tr), w)] = 0
+        return 0
+    elif (tuple(tr), w) in m:
+        return m[(tuple(tr), w)]
+    else:
+        vmax = 0
+        for t in tr:
+            if t[0] <= w:
+                v = thief(tr - {t}, w - t[0]) + t[1]
+                vmax = max(v, vmax)
+        m[(tuple(tr), w)] = vmax
+        return vmax
+
+
+print(thief(tr, 20))
