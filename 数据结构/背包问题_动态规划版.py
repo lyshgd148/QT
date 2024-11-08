4f30733536a72412f78be5013289a4ca38d93c86
12698 <lyshgd@gmail.com>
2024-11-08 22:32:16 +0800

    无

--- 数据结构/背包问题_动态规划版.py
+++ 数据结构/背包问题_动态规划版.py
@@ -1,7 +1,6 @@
 tr = [None, {'w': 2, 'v': 3}, {'w': 3, 'v': 4}, {'w': 4, 'v': 8}, {'w': 5, 'v': 8}, {'w': 9, 'v': 10}]
-weight=20
+weight = 20
 m = {(i, w): 0 for i in range(len(tr)) for w in range(weight + 1)}
-
 
 for i in range(1, len(tr)):
     for w in range(1, weight + 1):
@@ -11,6 +10,6 @@ for i in range(1, len(tr)):
             m[(i, w)] = max(m[(i - 1, w)], m[(i - 1, w - tr[i]['w'])] + tr[i]['v'])
 
 for i in range(len(tr)):
-    for w in range(weight+1):
-        print(m[(i,w)],end=" ")
+    for w in range(weight + 1):
+        print(m[(i, w)], end=" ")
     print("\n")
