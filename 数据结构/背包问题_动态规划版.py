1844a3cb7ea198634b87b4f82973aab7c5fba998
12698 <lyshgd@gmail.com>
2024-11-08 22:23:54 +0800

    无

---
+++ 数据结构/背包问题_动态规划版.py
@@ -0,0 +1,8 @@
+tr = [None, {'w': 2, 'v': 3}, {'w': 3, 'v': 4}, {'w': 4, 'v': 8}, {'w': 5, 'v': 8}, {'w': 9, 'v': 10}]
+m = {(i, w): 0 for i in range(len(tr)) for w in range(20 + 1)}
+
+for i in range(1, len(tr)):
+    for w in range(1, 20 + 1):
+        if tr[i]['w'] > w:
+
+        else:
