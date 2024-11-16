313f38bd623c57b5ed2387648440d6c3f54f9ef7
12698 <lyshgd@gmail.com>
2024-11-16 13:29:15 +0800

    无

---
+++ 数据结构/小端二进制.py
@@ -0,0 +1,27 @@
+import os
+
+
+def transform(num):
+    Snum = str(num)
+    if num < 100:
+        byte = int(Snum, 16)
+        low = bytes([int('0', 16)])
+        byte = bytes([byte])
+        byte = byte + low
+    else:
+        high = bytes([int(str(num // 100), 16)])
+        low = bytes([int(str(num % 100), 16)])
+        byte = low + high
+    return byte
+
+
+dir = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(dir, 'test.bin'), 'wb') as f:
+    for i in range(256):
+        f.write(transform(i))
+
+"""
+var = int('199', 16)
+print(var.to_bytes(2, 'little'), type(var))
+"""
