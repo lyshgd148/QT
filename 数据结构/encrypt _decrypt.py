47f676229dc8106ad04482c35cc25bc6d08ee7d5
12698 <lyshgd@gmail.com>
2024-11-21 22:39:40 +0800

    无

--- 数据结构/encrypt _decrypt.py
+++ 数据结构/encrypt _decrypt.py
@@ -1,4 +1,7 @@
-def encryption(string):
+import math
+
+
+def encrypt(string):
     import math
     num = math.sqrt(len(string))
     if num - int(num) > 0:
@@ -6,7 +9,7 @@ def encryption(string):
         for _ in range(num * num - len(string)):
             string += " "
     else:
-        num=int(num)
+        num = int(num)
 
     ls = [[None for _ in range(num)] for _ in range(num)]
     for i in range(num):
@@ -20,4 +23,17 @@ def encryption(string):
 
 
 def decrypt(string):
-    pass
+    num = int(math.sqrt(len(string)))
+    ls = [["" for i in range(num)] for j in range(num)]
+    strings = ""
+    for i in range(num):
+        for j in range(num):
+            ls[num - 1 - j][i] = string[i * num + j]
+    for i in range(num):
+        for j in range(num):
+            strings += ls[i][j]
+    return strings
+
+
+string = "!HEllo  world!!!"
+print(encrypt(string),"*****",decrypt(encrypt(string)))
