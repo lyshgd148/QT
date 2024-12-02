9cbdc66f5583c816e7a26a8e0489b38839f175cc
12698 <lyshgd@gmail.com>
2024-12-02 12:16:23 +0800

    无

--- My_GUI/PID.py
+++ My_GUI/PID.py
@@ -12,21 +12,30 @@ class Application(Frame):
     def createWidget(self):
         self.v1 = StringVar()
         self.v2 = StringVar()
-        Label(self.master, text="Kp: ").place(relx=0.22, rely=0.05)
-        Label(self.master, text="Kd: ").place(relx=0.22, rely=0.25)
+        self.v3 = StringVar()
+        self.v4 = StringVar()
+        Label(self.master, text="Kp: ").place(relx=0.22, rely=0)
+        Label(self.master, text="Kd: ").place(relx=0.22, rely=0.1)
+        Label(self.master, text="T: ").place(relx=0.22, rely=0.2)
+        Label(self.master, text="K: ").place(relx=0.22, rely=0.3)
 
         Button(self.master, text="确定", command=self.PID).place(relx=0.44, rely=0.4)
 
         self.Kp = 0.2
         self.Kd = 0.01
+        self.T = 1
+        self.K = 10
+
         self.v1.set(str(self.Kp))
         self.v2.set(str(self.Kd))
-        Entry(self.master, textvariable=self.v1).place(relx=0.3, rely=0.05)
-        Entry(self.master, textvariable=self.v2).place(relx=0.3, rely=0.25)
+        self.v3.set(str(self.T))
+        self.v4.set(str(self.K))
+        Entry(self.master, textvariable=self.v1).place(relx=0.3, rely=0.0)
+        Entry(self.master, textvariable=self.v2).place(relx=0.3, rely=0.1)
+        Entry(self.master, textvariable=self.v3).place(relx=0.3, rely=0.2)
+        Entry(self.master, textvariable=self.v4).place(relx=0.3, rely=0.3)
 
     def PID(self):
-        K = 10
-        T = 1
         self.Kp = float(self.v1.get())
         self.Kd = float(self.v2.get())
         y_0 = 10
@@ -37,7 +46,7 @@ class Application(Frame):
         t = [0]
         for i in range(1, 100):
             x = self.Kp * e_k + self.Kd * (e_k - e_last_k)
-            y.append(((K * x) + T * y[i - 1]) / (T + 1))
+            y.append(((self.K * x) + self.T * y[i - 1]) / (self.T + 1))
             e_last_k = e_k
             e_k = y_0 - y[i]
             t.append(i)
