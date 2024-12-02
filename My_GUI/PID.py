64fc709cbd8fce05c3bcc6ca1f13d833f300a5ca
12698 <lyshgd@gmail.com>
2024-12-02 12:07:40 +0800

    无

--- My_GUI/PID.py
+++ My_GUI/PID.py
@@ -15,16 +15,16 @@ class Application(Frame):
         Label(self.master, text="Kp: ").place(relx=0.22, rely=0.05)
         Label(self.master, text="Kd: ").place(relx=0.22, rely=0.25)
 
-        Button(self.master, text="确定", command=self.Ok).place(relx=0.44, rely=0.4)
+        Button(self.master, text="确定", command=self.PID).place(relx=0.44, rely=0.4)
 
-        self.Kp = 0.3
-        self.Kd = 0
+        self.Kp = 0.2
+        self.Kd = 0.01
         self.v1.set(str(self.Kp))
         self.v2.set(str(self.Kd))
         Entry(self.master, textvariable=self.v1).place(relx=0.3, rely=0.05)
         Entry(self.master, textvariable=self.v2).place(relx=0.3, rely=0.25)
 
-    def Ok(self):
+    def PID(self):
         K = 10
         T = 1
         self.Kp = float(self.v1.get())
@@ -44,30 +44,10 @@ class Application(Frame):
 
         plt.figure()
         plt.plot(t, y)
+        plt.text(0.88, 0.9, f'Kp:{self.Kp}', fontsize=10, color='red', transform=plt.gca().transAxes)
+        plt.text(0.88, 0.85, f'Kd:{self.Kd}', fontsize=10, color='red', transform=plt.gca().transAxes)
         plt.show()
 
-
-# K = 10
-# T = 1
-# Kp = 1.2
-# Kd = 0.1
-#
-# y_0 = 10
-# y = [0]
-# e_k = y_0 - 0
-# e_last_k = 0
-#
-# t = [0]
-# for i in range(1, 100):
-#     x = Kp * e_k + Kd * (e_k - e_last_k)
-#     y.append(((K * x) + T * y[i - 1]) / (T + 1))
-#     e_last_k = e_k
-#     e_k = y_0 - y[i]
-#     t.append(i)
-#
-# plt.figure()
-# plt.plot(t, y)
-# plt.show()
 
 if __name__ == "__main__":
     root = Tk()
