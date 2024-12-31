f762cca1f9f8ffa1ee05df3a230ab12b4160d9ec
12698 <lyshgd@gmail.com>
2024-12-31 11:07:44 +0800

    无

--- Turtle小海龟/迷宫效果.py
+++ Turtle小海龟/迷宫效果.py
@@ -1,6 +1,8 @@
 import turtle
 
 turtle.setup(500, 500)
+turtle.hideturtle()  # 隐藏海龟
+
 # 禁用动画
 turtle.tracer(0)
 
@@ -8,5 +10,10 @@ for i in range(150):
     turtle.forward(3*i)
     turtle.left(90)
 
-turtle.update()
+turtle.clear()
+for i in range(150):
+    turtle.forward(3*i)
+    turtle.right(90)
+
+# turtle.update()
 turtle.done()
